5ef08c3d2288442d3284621f87c2ccd4e70b201b
Taegwan Youn <tkyoun0421@naver.com>
2023-03-30 16:10:37 +0900

    select 3 : forEachm, for in 반복문

--- javascript/detail.js
+++ javascript/detail.js
@@ -1,10 +1,28 @@
+const pants = [28, 30, 32, 34];
+const shirt = [95, 100, 105, 110];
+
 $('.form-select').eq(0).on('input', function(e){
   let selectValue = e.currentTarget.value;
   if (selectValue == '셔츠'){
     $('.form-select').eq(1).removeClass('form-hide');
-  } else {
-    $('.form-select').eq(1).addClass('form-hide');
+    $('.form-select').eq(1).html('');
+    shirt.forEach(function(item){
+      $('.form-select').eq(1).append(`<option>${item}</option>`);
+    });
+  } else if (selectValue == '바지') {
+    $('.form-select').eq(1).removeClass('form-hide');    
+    $('.form-select').eq(1).html('');
+    pants.forEach(function(item) {
+      $('.form-select').eq(1).append(`<option>${item}</option>`);
+    });    
   }
 });
 
+var obj = {
+  name: 'kim',
+  age: 20
+}
 
+for (var key in obj){
+  console.log(obj[key]);
+}
