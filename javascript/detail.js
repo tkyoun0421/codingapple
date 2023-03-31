92a01a6a1a1ee38681b84dbbf477d5a097124eca
Taegwan Youn <tkyoun0421@naver.com>
2023-03-31 15:03:24 +0900

    array에 자주쓰는 sort, map, filter 함수

--- javascript/detail.js
+++ javascript/detail.js
@@ -4,6 +4,7 @@ var products = [
   { id: 2, price: 60000, title: 'Black Monastery'}
 ]
 
+
 let template = `
   <img src="https://via.placeholder.com/600" class="w-100">
   <h5>Card title</h5>
@@ -15,8 +16,8 @@ let count = 0;
 
 createDiv(products);
 
-document.querySelector('#more').addEventListener('click', function(){
-  count++;
+document.querySelector('#more').addEventListener('click', function(){  
+  count++;  
   if (count == 2) {
     document.querySelector('#more').style.display = 'none'
   }
@@ -32,6 +33,7 @@ document.querySelector('#more').addEventListener('click', function(){
 
 function createDiv(arr) {
   arr.forEach((item, i) => {
+    console.log(products);
     const div = document.createElement('div');
     div.classList.add('col-sm-4');
     div.innerHTML = template;
@@ -40,3 +42,9 @@ function createDiv(arr) {
     document.querySelectorAll('.col-sm-4 p')[arr[i].id].innerHTML = arr[i].price;
   })
 }
+
+document.querySelector('#price').addEventListener('click', function(){
+  products.sort(function(a,b){
+    return a.price - b.price
+  });
+});
